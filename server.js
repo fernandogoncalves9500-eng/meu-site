e4348b16dca58d8e2b61059bd0c57a1841a4f491
v62y <fernandogoncalves9500@gmail.com>
2026-08-12 23:00:50 -0400

    Atualizar o server.js

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("trust proxy", true);
+
 const acessos = [];
 
 app.use((req, res, next) => {
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0].trim() ||
+    req.headers["x-real-ip"] ||
+    req.socket.remoteAddress;
+
   acessos.push({
-    ip: req.ip,
+    ip: ip,
     horario: new Date().toISOString(),
     rota: req.originalUrl
   });
